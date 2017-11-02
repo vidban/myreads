e8940938fdb590b9c40046288883a1e2eeb99c47
Vidhu Bansal <liliput.j@gmail.com>
2017-11-02 13:16:14 -0700

    Added functionality to'Add Books' link on main page

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class BookList extends Component {
     render() {
@@ -159,7 +160,8 @@ class BookList extends Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link
+                to='/search'>Add a book</Link>
             </div>
           </div>
         )
