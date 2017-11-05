5d10dc2a7f64de373e50d3c9c8d4e8c31646b291
Vidhu Bansal <liliput.j@gmail.com>
2017-11-05 09:42:41 -0800

    Added a TODO for searchBooks.js

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -26,6 +26,9 @@ class SearchBooks extends Component {
         }
     }
 
+    // TODO: Manage book state such that books that are on my shelves show as being on that shelf in the search page
+    //         by using the prop updatedBookList and assigning shelf to the related book on the search page
+
     updateBooks = function(books) {
         if (books){
             let fbooks = books.filter((book) => book.imageLinks)
@@ -53,14 +56,6 @@ class SearchBooks extends Component {
                 className="close-search"
                 >Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input 
                     type="text" 
                     placeholder="Search by title or author"
