197c3982759d7d330d33afb66a071b74d870124a
Vidhu Bansal <liliput.j@gmail.com>
2017-11-04 17:55:26 -0700

    set option as selected to name of bookshelf in main page

--- src/BookList.js
+++ src/BookList.js
@@ -130,7 +130,7 @@ function bookShelf(shelf, booksInShelf){
                   <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail })` }}></div>
                       <div className="book-shelf-changer">
-                      <select>
+                      <select value={book.shelf}>
                           <option value="none" disabled>Move to...</option>
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
